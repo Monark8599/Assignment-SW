d7d270280e3e9c403354b6bf9fb4fffb814582d4
Monark <batbilegm.batbileg@gmail.com>
2023-12-01 17:26:47 +0800

    first commit

---
+++ dev-c++/Anhnii too.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int i, num, temp = 0; 
+    printf("Anhnii too mun ishiig shalgii gevel toogoo bichnuu: ");
+    scanf("%d", &num);
+    for (i = 2; i <= num / 2; i++)
+	{
+		if (num % i == 0){
+			temp++;
+			break;
+			}
+
+    }  
+    if (temp == 0 && num != 1){
+		printf("%d Anhnii too mun", num);
+	}else{
+    	printf("%d Anhnii too bish", num);
+	} 
+	return 0;
+}
