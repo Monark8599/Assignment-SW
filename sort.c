b3726ebf3f29b55c2fedd53d5b3b7540aeb9f673
Monark <batbilegm.batbileg@gmail.com>
2023-12-05 10:45:13 +0800

    third commit

---
+++ sort.c
@@ -0,0 +1,36 @@
+    #include <stdio.h>
+    int main(void)
+    {
+ 
+        int i, j, a, n, number[30];
+        printf("N iin utga \n");
+        scanf("%d", &n);
+ 
+        printf("Toonudaa ognu \n");
+        for (i = 0; i < n; ++i)
+            scanf("%d", &number[i]);
+ 
+        for (i = 0; i < n; ++i) 
+        {
+ 
+            for (j = i + 1; j < n; ++j)
+            {
+ 
+                if (number[i] > number[j]) 
+                {
+ 
+                    a =  number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+ 
+                }
+ 
+            }
+ 
+        }
+ 
+        printf("Usuj bui daraalal \n");
+        for (i = 0; i < n; ++i)
+            printf("%d\n", number[i]);
+ 
+    }
